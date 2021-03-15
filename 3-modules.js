49f1366ce2f1c63ce929425062a6b6fad97ade86
Igbadumhe <perfectimhetsike.igbadumhe@stanbicibtc.com>
2021-03-16 00:39:14 +0100

    export and require modules

---
+++ 3-modules.js
@@ -0,0 +1,8 @@
+//Modules
+const names = require('./4-names');
+const sayHi = require('./5-utils');
+
+
+sayHi(names.john)
+sayHi(names.perfect)
+sayHi(names.salami);
---
+++ 4-names.js
@@ -0,0 +1,10 @@
+//secret
+const secret = "super secret";
+//share
+const john = "john"
+const perfect = "perfect"
+const salami = "salami"
+
+//console.log(module)
+
+module.exports = {john, perfect, salami}
