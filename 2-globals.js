3ea17f22feed8e7fa18b97c3e381e362297851ff
Igbadumhe <perfectimhetsike.igbadumhe@stanbicibtc.com>
2021-03-16 00:19:15 +0100

    globals explained

---
+++ 2-globals.js
@@ -0,0 +1,12 @@
+//Global  - No Window
+
+// ___dirname - parth to current directory
+//___filename  - file name
+// require - function to use modules or import modules
+//moule  -  info about current module
+//process  -  info about the env where the program is executed where your app is deployed
+
+console.log(__dirname)
+setInterval(()=> {
+    console.log("Hello world")
+}, 1000)
